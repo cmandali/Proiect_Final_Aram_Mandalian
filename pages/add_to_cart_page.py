53af0e33ff77b0d5ab96bf5698dc6f2282634761
cmandali <153735017+cmandali@users.noreply.github.com>
2024-03-06 14:56:51 +0200

    Update add_to_cart_page.py

--- pages/add_to_cart_page.py
+++ pages/add_to_cart_page.py
@@ -1,93 +1,59 @@
 import time
-
+from pages.base_page import BasePage
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from pages.login_page import LoginPage
 
 
-class Add_to_Cart(LoginPage):
-    ADD_TO_CART_BUTTONS = (By.CLASS_NAME, "btn")
-    SHOPPING_BASKET_BUTTONS = (By.ID, "shopping_cart_container")
-    CHECKOUT_BUTTON = (By.ID, "checkout")
-    FIRST_NAME_INPUT = (By.ID, "first-name")
-    LAST_NAME_INPUT = (By.ID, "last-name")
-    POSTAL_CODE_INPUT = (By.ID, "postal-code")
-    CONTINUE_BUTTON = (By.ID, "continue")
-    ITEMS_IN_BASKET = (By.CLASS_NAME, "inventory_item_price")
-    TAX_VALUE = (By.CLASS_NAME, "summary_tax_label")
-    TOTAL_AMOUNT_INCL_TAXES = (By.CLASS_NAME, "summary_total_label")
-    ALL_ITEMS_BUTTON = (By.ID, "inventory_sidebar_link")
-    REMOVE_ITEMS = (By.XPATH, '//div/button[text()="Remove"]')
+class LoginPage(BasePage):
+    LOGIN_PAGE_URL = "https://www.saucedemo.com/"
+    USER_NAME_BOX = (By.ID, "user-name")
+    PASSWORD_BOX = (By.ID, "password")
+    LOGIN_BUTTON = (By.CSS_SELECTOR, "input[name='login-button']")
+    RESET_FIELDS = (By.CSS_SELECTOR, 'svg[data-icon="times"]')
+    ERROR_TEXT = (By.XPATH, '//h3[text()="Epic sadface: Username and password do not match any user in this service"]')
+    PASSWORD_REQUIRED_TEXT = (By.XPATH, '//h3[text()="Epic sadface: Password is required"]')
+    MENU_BUTTON = (By.ID, "react-burger-menu-btn")
+    LOGOUT_BUTTON = (By.ID, "logout_sidebar_link")
 
-    def all_items_page(self):
-        self.open()
-        self.enter_name("standard_user")
-        self.enter_password("secret_sauce")
-        self.click_login()
+    def open(self):
+        self.driver.get(self.LOGIN_PAGE_URL)
+
+    def check_url(self, expected_url):
         time.sleep(1)
+        actual_url = self.driver.current_url
+        assert actual_url == expected_url, "Not the correct URL"
 
-    def add_to_cart(self, number_of_products):
-        items = self.find_more_than_one(self.ADD_TO_CART_BUTTONS)
-        for btn in items:
-            if btn in items[0:int(number_of_products)]:
-                btn.click()
-            else:
-                pass
+    def enter_name(self, user_name):
+        if self.find(self.USER_NAME_BOX).get_attribute("value"):
+            self.find(self.USER_NAME_BOX).clear()
+            self.type(self.USER_NAME_BOX, user_name)
+        else:
+            self.type(self.USER_NAME_BOX, user_name)
 
-    def navigate_to_shopping_basket(self):
-        self.find(self.SHOPPING_BASKET_BUTTONS).click()
+    def enter_password(self, password):
+        if self.find(self.PASSWORD_BOX).get_attribute("value"):
+            self.find(self.PASSWORD_BOX).clear()
+            self.type(self.PASSWORD_BOX, password)
+        else:
+            self.type(self.PASSWORD_BOX, password)
+        time.sleep(2)
 
-    def proceed_to_checkout(self):
-        checkout = WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located(self.CHECKOUT_BUTTON))
-        checkout.click()
+    def click_login(self):
+        login = WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located(self.LOGIN_BUTTON))
+        login.click()
+        time.sleep(2)
 
-    def complete_form(self, first_name, last_name, postal_code):
-        self.type(self.FIRST_NAME_INPUT, first_name)
-        self.type(self.LAST_NAME_INPUT, last_name)
-        self.type(self.POSTAL_CODE_INPUT, postal_code)
+    def check_error_text(self, expected_text):
+        error_text = self.find(self.ERROR_TEXT).text
+        assert expected_text in error_text
 
-    def continue_to_checkout(self):
-        proceed = WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located(self.CONTINUE_BUTTON))
-        proceed.click()
+    def reset_login_fields(self):
+        self.find(self.RESET_FIELDS).click()
 
-    def check_number_of_products(self, number):
-        items_in_basket = self.find_more_than_one(self.ITEMS_IN_BASKET)
-        assert len(items_in_basket) == int(number), "Unmatching number of products"
-
-    def get_the_prices(self):
-        items_in_basket = self.find_more_than_one(self.ITEMS_IN_BASKET)
-        items_text = []
-        for item in items_in_basket:
-            items_text.append(item.text)
-
-        items_prices = []
-        for prices in items_text:
-            items_prices.append(prices.replace("$", ""))
-
-        items_prices_floats = []
-        for price_value in items_prices:
-            items_prices_floats.append(float(price_value))
-
-        return items_prices_floats
-
-    def verify_total_amount(self):
-        items_prices_floats = self.get_the_prices()
-        tax = self.find(self.TAX_VALUE).text
-        tax_value = float(tax.replace("Tax: $", ""))
-        total_items_value = sum(items_prices_floats)
-        total_amount = self.find(self.TOTAL_AMOUNT_INCL_TAXES).text
-        total_amount_value = float(total_amount.replace("Total: $", ""))
-        assert tax_value + total_items_value == total_amount_value, "Total not correct"
-
-    def navigate_to_all_items_page(self):
+    def log_out(self):
         self.find(self.MENU_BUTTON).click()
-        self.find(self.ALL_ITEMS_BUTTON).click()
+        self.find(self.LOGOUT_BUTTON).click()
 
-    def remove_items(self, item_removed):
-        items_removed = self.find_more_than_one(self.REMOVE_ITEMS)
-        for btn_remove in items_removed:
-            if btn_remove in items_removed[0:int(item_removed)]:
-                btn_remove.click()
-            else:
-                pass
+    def check_visibility_of_fields(self):
+        assert self.find(self.USER_NAME_BOX).is_displayed(), "Fields are not visible"
